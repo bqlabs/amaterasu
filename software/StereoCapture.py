26edb44d2878b0e4430edcd952457f3045ea00b8
David-Estevez <david.estevez.fdez@gmail.com>
2015-10-19 12:52:40 +0200

    Add template for stereo capture

---
+++ software/StereoCapture.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+__author__ = 'def'
+
+left_camera_device = '/dev/video0'
+right_camera_device = '/dev/video1'
+
+webcam_resolution = (640, 480)
+viewer_resolution = (1920, 180)
+
+def capture_stereo_pair(left_camera, right_camera):
+    left_picture = None
+    right_picture = None
+    return left_picture, right_picture
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
