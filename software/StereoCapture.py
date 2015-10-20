51aeee99db19233f550208edbdc59ab5267b65d7
David-Estevez <david.estevez.fdez@gmail.com>
2015-10-20 17:15:31 +0200

    Add function to record stereo photos ready to be used in a HMD

--- software/StereoCapture.py
+++ software/StereoCapture.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 __author__ = 'def'
 
@@ -10,7 +11,7 @@ left_camera_inverted = False
 right_camera_inverted = True
 
 webcam_resolution = (640, 480)
-viewer_resolution = (1920, 180)
+viewer_resolution = (1920, 1080)
 
 def capture_stereo_pair(left_camera, right_camera):
     l_status = left_camera.grab()
@@ -45,8 +46,12 @@ def capture_stereo_pair_without_sync(left_camera, right_camera):
 
 def main():
     left_camera = cv2.VideoCapture(1)
+    left_camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1024)
+    left_camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 768)
     # left_camera.open()
     right_camera = cv2.VideoCapture(2)
+    right_camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1024)
+    right_camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 768)
     # right_camera.open()
 
     if not left_camera.isOpened():
@@ -70,14 +75,32 @@ def main():
         print "Timeout reading cameras"
 
     left_picture, right_picture = capture_stereo_pair(left_camera, right_camera)
-
-    combined_image = np.concatenate((left_picture, right_picture), axis=1)
+    rotated_right = np.rot90(right_picture, 2)
 
     cv2.imshow('Left image', left_picture)
-    cv2.imshow('Right image', right_picture)
+    cv2.imshow('Right image', rotated_right)
+
+    # Scale images
+    factor = viewer_resolution[1] / float(webcam_resolution[1])
+    left_resized = cv2.resize(left_picture, None, fx=factor, fy=factor, interpolation=cv2.INTER_CUBIC)
+    right_resized = cv2.resize(rotated_right, None, fx=factor, fy=factor, interpolation=cv2.INTER_CUBIC)
+
+    # Calculate optic axis (Approximately)
+    left_center = (left_resized.shape[0]/2, left_resized.shape[1]/2)
+    right_center = (right_resized.shape[0]/2, right_resized.shape[1]/2)
+
+    # Concatenate images (based on optical center):
+    half_size_eye = viewer_resolution[0]/4
+    combined_image = np.concatenate((left_picture[:,left_center[0]-half_size_eye:left_center[0]+half_size_eye, :],
+                                     rotated_right[:, right_center[0]-half_size_eye:right_center[0]+half_size_eye, :]),
+                                    axis=1)
+
+
     cv2.imshow('Stereo pair', combined_image)
     cv2.waitKey(-1)
     cv2.destroyAllWindows()
+
+    cv2.imwrite(time.strftime("stereo-%d_%m_%y-%H_%M_%S.png"), combined_image)
 
     left_camera.release()
     right_camera.release()
